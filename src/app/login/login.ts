8b46e5ef2defc70b1363554117e68cb7d40e3eb0
jaykoshta <koshtajay0108@gmail.com>
2026-09-05 13:37:34 +0530

    app config

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../services/auth';
 
 @Component({ selector: 'app-login', imports: [FormsModule, RouterLink], templateUrl: './login.html', styleUrl: './login.css' })
 export class LoginComponent {
@@ -10,7 +11,8 @@ export class LoginComponent {
   protected error = signal('');
   protected emailError = signal('');
 
-  constructor(private readonly router: Router) {}
+  // constructor(private readonly router: Router) {}
+  constructor(private readonly router: Router, private readonly auth: Auth) {}
 
   protected signIn(): void {
     const normalizedEmail = this.email.trim();
@@ -34,6 +36,23 @@ export class LoginComponent {
 
     this.emailError.set('');
     this.error.set('');
-    this.router.navigate(['/home'], { state: { email: normalizedEmail } });
+    // this.router.navigate(['/home'], { state: { email: normalizedEmail } });
+    this.auth.login(normalizedEmail, this.password).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+
+        this.router.navigate(['/home'], {
+          state: { email: response.user.email }
+        });
+      },
+
+      error: (error) => {
+        console.log('Login failed:', error);
+
+        this.error.set(
+          error.error?.error || 'Invalid email or password.'
+        );
+      }
+    });
   }
 }
---
+++ src/app/services/auth.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface LoginResponse {
+  message: string;
+  user: {
+    email: string;
+  };
+  token: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth {
+
+  private apiUrl = 'http://localhost:3000/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(
+      `${this.apiUrl}/login`,
+      {
+        email: email,
+        password: password
+      }
+    );
+  }
+}
